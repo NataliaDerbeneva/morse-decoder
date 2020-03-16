564e863af9fe3c242a82d3e69c9c6170105f881e
Natalia Derbeneva <nat_ff@mail.ru>
2020-03-17 01:12:55 +0300

    init: start 'morse-decoder' task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let i=0;
+    let j;
+    let word,symb;
+    let numLetters;
+    let outStr = "";
+  
+    while(i<expr.length){
+      word = (expr.slice(i).match(/[0,1]+/))[0];
+      numLetters = word.length/10;
+      for(let j=0; j<numLetters;j++){
+        symb = word.substring(j*10,(j+1)*10);
+        symb = symb.replace(/10/g,".");
+        symb = symb.replace(/11/g,"-");
+        symb = symb.replace(/0/g,"");
+        outStr += MORSE_TABLE[symb];
+      }
+      i+=word.length;
+      if(i<expr.length){
+        i += 10;
+        outStr += " ";
+      }  
+    }    
+  
+    return outStr;
+  }
 
 module.exports = {
     decode
